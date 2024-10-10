407a17ea6f4cd1f6f7af7990220bb0b601666e98
emmyalexanyan <emmy.alexanyan@gmail.com>
2024-10-10 07:43:20 +0400

    singleton

---
+++ Timer.cpp/Timer.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <memory>
+#include <ctime>
+
+class Timer{
+    private:
+        Timer(){
+            std::cout<<"Timer created\n";
+            startTime = clock();
+        }
+
+        Timer(const Timer&) = delete;
+        Timer& operator=(const Timer&) = delete;
+
+        Timer(Timer&&) = delete;
+        Timer& operator=(Timer&&) = delete;
+
+        static std::shared_ptr<Timer> instance;
+        clock_t startTime;
+
+    public:
+        static std::shared_ptr<Timer> getInstance() {
+            if(!instance){
+                instance = std::shared_ptr<Timer>(new Timer());
+            }
+            return instance;
+        }
+
+        double elapsedTime(){
+            clock_t endTime = clock();
+            return (double)(endTime - startTime) / CLOCKS_PER_SEC;
+        }
+
+};
+std::shared_ptr<Timer> Timer::instance = nullptr;
+
+int main(){
+    std::shared_ptr<Timer> timer = Timer::getInstance();
+    int s = 0;
+    for(int i = 0; i < 100; i++){
+        s += i;
+    }
+    std::cout<<"S="<<s<<std::endl;
+    std::cout<<"ElapsedTime:"<<timer->elapsedTime()<<"seconds"<<std::endl;
+    return 0;
+}
